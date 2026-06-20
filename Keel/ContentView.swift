00e01ee092873bc3c1be9b9933e4e64f018c9361
Ngonidzashe Mangudya <imngonii@gmail.com>
2026-06-20 16:47:34 +0200

    Initial Commit

---
+++ Keel/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Keel
+//
+//  Created by Ngonidzashe  Mangudya on 2026/06/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Keel/KeelApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeelApp.swift
+//  Keel
+//
+//  Created by Ngonidzashe  Mangudya on 2026/06/20.
+//
+
+import SwiftUI
+
+@main
+struct KeelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
